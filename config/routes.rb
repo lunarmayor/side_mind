c81af634f4da9ccdcfcc03b9f4b545e7877ee052
Michael Morrissey <mikehmorrissey@gmail.com>
2013-11-26 18:21:23 -0600

    fix root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 SideMind::Application.routes.draw do
   devise_for :users
-  root 'user#show'
+  root 'users#show'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

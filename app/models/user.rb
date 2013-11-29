4e9d765c36249601413928792cadefe633f7c389
Michael Morrissey <mikehmorrissey@gmail.com>
2013-11-29 12:21:16 -0600

    add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  validates :email, :name, :phone, presence: true
+  validates :email, :phone, uniqueness: {:case_sensitive => false}
+  validates :password, length: {within: 8..20}
 end
